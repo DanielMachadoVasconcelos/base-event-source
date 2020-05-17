24c588963d5cfc424e00290f2ffaadf559564caa
Daniel <danielm.vasconcelos@gmail.com>
2020-05-17 16:55:25 -0300

    Changed to new aws sdk

--- src/main/java/br/com/ead/eventsource/config/DynamoDBConfiguration.java
+++ src/main/java/br/com/ead/eventsource/config/DynamoDBConfiguration.java
@@ -1,43 +1,36 @@
 package br.com.ead.eventsource.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
-import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 
-import static com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
-import static com.amazonaws.regions.Regions.US_WEST_2;
+import java.net.URI;
+
+import static software.amazon.awssdk.regions.Region.*;
 
 @Configuration
-@EnableDynamoDBRepositories(basePackages = "br.com.ead.eventsource.repositories")
 public class DynamoDBConfiguration {
 
-    @Value("${amazon.aws.awsAccessKey}")
+    @Value("${amazon.aws.accesskey}")
     private String awsAccessKey;
 
-    @Value("${amazon.aws.accesskey}")
+    @Value("${amazon.aws.secretkey}")
     private String awsSecretKey;
 
     @Value("${amazon.dynamodb.endpoint}")
     private String dynamoDbEndpoint;
 
+
     @Bean
-    public DynamoDBMapper dynamoDBMapper() {
-        AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)))
-                .withRegion(US_WEST_2);
-
-        if (!StringUtils.isEmpty(dynamoDbEndpoint)) {
-            builder.setEndpointConfiguration(new EndpointConfiguration(dynamoDbEndpoint, US_WEST_2.getName()));
-        }
-
-        return new DynamoDBMapper(builder.build(), DynamoDBMapperConfig.DEFAULT);
+    public DynamoDbAsyncClient dynamoDbAsyncClient() {
+        return DynamoDbAsyncClient.builder()
+                .region(US_EAST_2)
+                .endpointOverride(URI.create(dynamoDbEndpoint))
+                .credentialsProvider(DefaultCredentialsProvider
+                        .builder()
+                        .build())
+                .build();
     }
-
 }
--- src/main/java/br/com/ead/eventsource/repositories/AccountRepository.java
+++ src/main/java/br/com/ead/eventsource/repositories/AccountRepository.java
@@ -1,11 +1,42 @@
 package br.com.ead.eventsource.repositories;
 
 import br.com.ead.eventsource.model.Account;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.springframework.data.repository.Repository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemResponse;
 
-@EnableScan
-public interface AccountRepository extends Repository<Account, String> {
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
+import static software.amazon.awssdk.services.dynamodb.model.AttributeValue.*;
 
+@Log4j2
+@Repository
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountRepository  {
+
+    private DynamoDbAsyncClient client;
+
+    @Value("${amazon.aws.tableName}")
+    private String tableName;
+
+    public CompletableFuture<PutItemResponse> openAccount(Account account){
+        Builder builder = builder();
+        return client.putItem(PutItemRequest.builder().item(
+                        Map.of( "id", builder.s(account.getId()).build(),
+                                "status", builder.s("CREATED").build(),
+                                "createdAt", builder.s(LocalDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_LOCAL_DATE)).build(),
+                                "amount", builder.s(account.getAmount().toString()).build()))
+                        .tableName(tableName)
+                        .build());
+    }
 }
